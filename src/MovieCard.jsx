3fb622431ec60c2c6f88181dfd3e656bfe0a8ea9
Anagharanik <anagharanikilled2000@gmail.com>
2024-10-21 17:16:27 +0530

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,21 +1,5 @@
 
-// import './MovieCard.css';
 
-// const MovieCard = ({ movie, onSelect }) => {
-//     return (
-//         <div className="movie-card" onClick={() => onSelect(movie)}>
-//             <img 
-//                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-//                 alt={movie.title} 
-//             />
-//             <h3>{movie.title}</h3>
-//             <p>Release Date: {movie.release_date}</p>
-//             <p>Rating: {movie.vote_average}</p>
-//         </div>
-//     );
-// };
-
-// export default MovieCard;
 
 import './styles/MovieCard.css';
 
